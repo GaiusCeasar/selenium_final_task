d982ae255dfa87687a7d162fa04fea997baa5256
king <ivakmaksim@yandex.ru>
2023-12-04 09:04:18 +0300

    add new

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,11 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    FORM_LOGIN_EMAIL = (By.ID, "id_login-username")
+    FORM_LOGIN_PASS = (By.ID, "id_login-password")
+    FORM_REGISTRATION_EMAIL = (By.ID, "id_registration-email")
+    FORM_REGISTRATION_PASS = (By.ID, "id_registration-password1")
+    FORM_REGISTRATION_CONFIRM_PASS = (By.ID, "id_registration-password2")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "в строке нет login"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
